136595de805daba352fde9bd8007296687c90e84
Thulasi <thulasisushma93@gmail.com>
2023-11-28 15:12:53 +0100

    Day2-updated all routers, all controllers, books controllers

--- Backend/controllers/booksController.js
+++ Backend/controllers/booksController.js
@@ -33,4 +33,45 @@ export const getBookById=async (req,res,next)=>{
     }
 }
 
-//create, update, delete
+export const createBook = async (req, res,next)=>{
+    try {
+        const newBook = await Book.create(req.body);
+
+        if(newBook){
+            res.send({success:true, data:newBook})
+        }else{
+            res.send({success:false})
+        }
+    } catch (err) {
+        next(err)
+    }
+}
+
+export const updateBook = async (req,res,next)=>{
+
+    try {
+        const updateOne = await Book.findByIdAndUpdate(req.params.id, req.body, {new:true});
+
+        if(updateOne){
+            res.send({success:true, data:updateOne})
+        }else{
+            res.send({success:false})
+        }
+    } catch (err) {
+        next(err)
+    }
+}
+
+export const deleteBook = async(req,res,next)=>{
+    try {
+        const deleteOne = await Book.findByIdAndDelete(req.params.id)
+
+        if(deleteOne){
+            res.send({success:true, data:deleteOne})
+        }else{
+            res.send({success:false})
+        }
+    } catch (err) {
+        next(err)
+    }
+}
